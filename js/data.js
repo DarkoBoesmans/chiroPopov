e7fb6ef72ac04e3bb89b902626ce336cad033d04
Darko Boesmans <darkoboesmans@gmail.com>
2024-04-27 17:14:29 +0200

    fix: data.js toegevoegd voor dynamisch elementen te genereren

---
+++ js/data.js
@@ -0,0 +1,93 @@
+const arrMededelingen = [
+	{
+		id: 1,
+		leider: "Robbe Van Riet",
+		url_afbeelding: "robbe.jpg",
+		mededeling: "Vergeet zeker niet om je in te schrijven voor de bosfeesten!",
+	},
+	{
+		id: 2,
+		leider: "Kevin Thiry",
+		url_afbeelding: "kevin.jpg",
+		mededeling:
+			"Bekijk zeker ook de leiding van de verschillende groepen op de afdelingspagina!",
+	},
+	{
+		id: 3,
+		leider: "Darko Boesmans",
+		url_afbeelding: "darko.jpg",
+		mededeling:
+			"Het Krakmoment is net geweest, vraag zeker aan de leiding voor info rond he kamp en de kampbezoeken.",
+	},
+	{
+		id: 4,
+		leider: "Robin De Croock",
+		url_afbeelding: "robin.jpg",
+		mededeling: "Onze verbeterde website staat online!",
+	},
+];
+
+const arrLeiders = [
+	{
+		id: 1,
+		naam: "Darko Boesmans",
+		url_afbeelding: "darko.jpg",
+		afdeling: "Pinkels",
+		nummer: "+324 84 38 11 60",
+	},
+];
+
+const arrAfdelingen = [
+	{
+		id: 1,
+		naam: "Pinkels",
+		leeftijd: "6-8 Jaar",
+		omschrijving: `
+            De allerkleinsten maar met het grootste enthousiasme. Sommigen komen met vriendjes uit hun school, familie of helemaal alleen, maar allemaal deel van onze groep. Hoewel ze de jongsten zijn durven ze soms meer dan onze oudere groepen. Op zondag laten ze zich gaan met mini-avonturen, knutselen, spannende raadsels en moppen tappen.
+
+            Samen met hun leiding groeien ze op tot echte helden en maken ze veel nieuwe vrienden. Ze gaan hier ook op hun eerste daguitstap zonder ouders (meestal moeilijker voor de mama en papa dan voor hun zelf ;-) ) en durven misschien ook al op kamp te gaan. 
+        `,
+	},
+	{
+		id: 2,
+		naam: "Speelclub",
+		leeftijd: "8-10 Jaar",
+		omschrijving: `
+            Dit zijn één van de jongsten in de Chiro. Ze komen elke zondag weer om te ravotten en om samen met hun leiding allerlei spannende avonturen te beleven. Voor hen is het kamp meestal de eerste keer dat ze zonder hun ouders voor 10 dagen weg zijn, maar zij bewijzen keer op keer dat zij, als jongsten, ook hun mannetje (vrouwtje) kunnen staan.
+
+            Samen met hun leiding duiken ze onder in een fantasiewereld van bosberen en chocoprinsen, spelen ze poppenkast of verkleden ze zich in een heks. Ze trekken al eens op een tocht vol spelletjes, plezante liedjes en gekke opdrachten. 
+        `,
+	},
+	{
+		id: 3,
+		naam: "Rakwi",
+		leeftijd: "10-12 Jaar",
+		omschrijving: `
+            Deze stoere jongens en meisjes worden bij de rakwileiding verwacht om samen met hen het aapje uit te hangen, of om de gekste creaturen in klei te maken. Ze ravotten zich doorgaans te pletter, trekken er al eens op uit met de fiets en lopen zich de ziel uit hun lijf bij een supergroot stratego-bosspel. In het geniep lachen de meisjes met de jongens, of vertellen ze al eens een vuile mop.  
+        `,
+	},
+	{
+		id: 4,
+		naam: "Tito",
+		leeftijd: "12-14 Jaar",
+		omschrijving: `
+            De Tito’s maken al eens een daguitstap met de fiets of met de trein om in één of andere stad een groot zoekspel te doen. Pleinspelen vinden ze ook best leuk, vooral wanneer er wat tactiek aan te pas komt. Soms schminken ze zich om ter gekst, en in de zomer kappen ze hun leiding nat. 
+        `,
+	},
+	{
+		id: 5,
+		naam: "Keti",
+		leeftijd: "14-16 Jaar",
+		omschrijving: `
+            De Keti’s trekken op avontuur, doen een tweedaagse tocht met de rugzak, of bouwen zelf een vlot om er dan een vijver mee af te varen. Ze beulen zich ook wel eens af in een zelfverzonnen sport, of zetten hun lokaal volledig op z’n kop. Wie weet maken ze wel een film waarin ze zelf schitteren als acteur.  
+        `,
+	},
+	{
+		id: 6,
+		naam: "Aspi",
+		leeftijd: "16-18 Jaar",
+		omschrijving: `
+            De Aspi’s nemen het er goed van en wisselen hevige spelen af met stevige babbels in het gezelligste hoekje van het lokaal. Op gezette tijden springen ze de leiding van een jongere afdeling bij. Zo proeven ze wat het is om leiding te zijn, want dat is voor hen zeer nabije toekomst. Samen met hun leiding riskeren ze zich aan het touwenparcours of nemen ze deel aan een workshop. Op grote evenementen kom je heel wat aspi-ploegen tegen. Zelf organiseren ze een heuse aspi-fuif.   
+        `,
+	},
+];
