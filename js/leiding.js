527ca46d5278ce202dd9a6f2f09d2347ae43ecef
Darko Boesmans <darkoboesmans@gmail.com>
2024-04-27 17:21:49 +0200

    fix: data.js opgedeeld in verschillende bestanden

---
+++ js/leiding.js
@@ -0,0 +1,9 @@
+const arrLeiders = [
+	{
+		id: 1,
+		naam: "Darko Boesmans",
+		url_afbeelding: "darko.jpg",
+		afdeling: "Pinkels",
+		nummer: "+324 84 38 11 60",
+	},
+];
---
+++ js/mededeling.js
@@ -0,0 +1,28 @@
+const arrMededelingen = [
+	{
+		id: 1,
+		leider: "Robbe Van Riet",
+		url_afbeelding: "robbe.jpg",
+		mededeling: "Vergeet zeker niet om je in te schrijven voor de bosfeesten!",
+	},
+	{
+		id: 2,
+		leider: "Kevin Thiry",
+		url_afbeelding: "kevin.jpg",
+		mededeling:
+			"Bekijk zeker ook de leiding van de verschillende groepen op de afdelingspagina!",
+	},
+	{
+		id: 3,
+		leider: "Darko Boesmans",
+		url_afbeelding: "darko.jpg",
+		mededeling:
+			"Het Krakmoment is net geweest, vraag zeker aan de leiding voor info rond he kamp en de kampbezoeken.",
+	},
+	{
+		id: 4,
+		leider: "Robin De Croock",
+		url_afbeelding: "robin.jpg",
+		mededeling: "Onze verbeterde website staat online!",
+	},
+];
